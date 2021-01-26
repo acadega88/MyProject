d0ef21802280f9aa233a0ec06d49c43f1bc18909
Aleksandar Dega <32138184+acadega88@users.noreply.github.com>
2021-01-26 09:15:48 +0100

    Update LocaleStrings.java

--- src/main/java/lib/strings/LocaleStrings.java
+++ src/main/java/lib/strings/LocaleStrings.java
@@ -2,33 +2,4 @@ package lib.strings;
 
 public class LocaleStrings {
 
-    //Welcome page
-    public static final String WELCOME_PAGE_DESCRIPTION = "Access Measurements and Documentation Data Across Devices";
-    public static final String LOGIN_PAGE_DESCRIPTION = "Login with your Bosch ID";
-
-    //Upgrade page
-    public static final String UPGRADE_PAGE_DESCRIPTION = "The Standard Plan";
-    public static final String YEARLY_PRICE = "€ 149";
-    public static final String MONTHLY_PRICE = "€ 178.90 or 14.90 monthly";
-
-    // Home page
-    public static final String HOME_PAGE_DESCRIPTION = "Welcome back";
-    public static final String GRID_VIEW = "Grid";
-    public static final String LIST_VIEW = "List";
-    public static final String MAP_VIEW = "Map";
-    public static final String PROJECT_OBJECT = "project";
-    public static final String CANVAS_OBJECT = "canvas";
-    public static final String MESSAGE = "Lost Internet connection. As long as the internet connection is lost no data will be synced.";
-
-    //FAQ page
-    public static final String HEADING2 = "MeasureOn Cloud";
-
-    //PRO360 Page
-    public static final String HEADING1 = "PRO360\nYOUR DIGITAL\nCONTROL CENTER";
-
-    // ManageAccountPage
-    public static final String MANAGE_ACCOUNT_PAGE_DESCRIPTION = "Your Account";
-    public static final String PRO_USER = "MeasureOn Pro";
-    public static final String FREE_USER = "Standard Plan";
-
 }
